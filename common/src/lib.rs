0d83c559a8d6a11f1fe3c591e68f819a3f25bb08
Ben Dayan <ben.dayan@gehealthcare.com>
2024-12-02 00:50:28 +0200

    day1 - parts 1 and 2

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,8 +3,11 @@ use std::io::{self, BufRead};
 use std::path::Path;
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+pub fn read_lines<P>(filename: P) -> io::Result<impl Iterator<Item = String>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    let strings = io::BufReader::new(file).lines().map(|x| x.unwrap());
+    Ok(strings)
 }
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,50 @@
+use std::{collections::HashMap, iter::zip};
+
+use common::read_lines;
+
+fn part1() {
+    let pairs = read_lines("./day01/input").unwrap().map(|x| {
+        x.split_whitespace()
+            .map(|s| s.to_owned().parse::<i32>().unwrap())
+            .collect::<Vec<_>>()
+    });
+    let mut first_list = Vec::<i32>::new();
+    let mut second_list = Vec::<i32>::new();
+    for p in pairs {
+        first_list.push(p[0]);
+        second_list.push(p[1]);
+    }
+    first_list.sort();
+    second_list.sort();
+    let sum_diff = zip(first_list, second_list)
+        .map(|p| p.0.abs_diff(p.1))
+        .sum::<u32>();
+
+    println!("{:?}", sum_diff);
+}
+
+fn part2() {
+    let pairs = read_lines("./day01/input").unwrap().map(|x| {
+        x.split_whitespace()
+            .map(|s| s.to_owned().parse::<i32>().unwrap())
+            .collect::<Vec<_>>()
+    });
+    let mut first_list = Vec::<i32>::new();
+    let mut second_map = HashMap::<i32, i32>::new();
+    for p in pairs {
+        first_list.push(p[0]);
+        *second_map.entry(p[1]).or_insert(0) += 1;
+    }
+    // first_list.sort();
+    // second_list.sort();
+    let mut similarity_score = 0;
+    for loc in first_list {
+        similarity_score += *second_map.entry(loc).or_insert(0) * loc;
+    }
+    println!("{:?}", similarity_score);
+}
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+    part2();
 }
