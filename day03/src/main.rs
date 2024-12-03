b3c03ff862697cc5c875feb630fb5c9f849e14ff
Ben Dayan <ben.dayan@gehealthcare.com>
2024-12-03 07:30:59 +0200

    feat: day3 - part1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,24 @@
+use common::read_lines;
+
+use regex::Regex;
+
+fn part1() {
+    let instructions = read_lines("./day03/input").unwrap().collect::<String>();
+    let mul_pattern = r"mul\(([0-9]+),([0-9]+)\)";
+    let mul_re = Regex::new(&mul_pattern).unwrap();
+    let mul_iter = mul_re.captures_iter(&instructions).map(|m| m.extract());
+    let mut total = 0;
+
+    for (_, [a, b]) in mul_iter {
+        total += (a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap());
+    }
+
+    println!("{:?}", total);
+}
+
+fn part2() {}
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+    part2();
 }
