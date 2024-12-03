3fb9a6515b58b28feeeaa5b2278cf1083455debf
Ben Dayan <ben.dayan@gehealthcare.com>
2024-12-03 07:56:06 +0200

    feat: day3 - part2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -10,13 +10,37 @@ fn part1() {
     let mut total = 0;
 
     for (_, [a, b]) in mul_iter {
-        total += (a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap());
+        total += a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
     }
 
     println!("{:?}", total);
 }
 
-fn part2() {}
+fn part2() {
+    let instructions = read_lines("./day03/input").unwrap().collect::<String>();
+    let mul_pattern = r"don't|do|mul\(([0-9]+),([0-9]+)\)";
+    let mul_re = Regex::new(&mul_pattern).unwrap();
+    let mul_iter = mul_re.captures_iter(&instructions);
+    let mut total = 0;
+
+    let mut enabled_factor = 1;
+    for a in mul_iter {
+        match a.get(0).unwrap().as_str() {
+            "don't" => {
+                enabled_factor = 0;
+            }
+            "do" => {
+                enabled_factor = 1;
+            }
+            _ => {
+                total += (a.get(1).unwrap().as_str().parse::<i32>().unwrap()
+                    * a.get(2).unwrap().as_str().parse::<i32>().unwrap())
+                    * enabled_factor;
+            }
+        };
+    }
+    println!("{:?}", total);
+}
 
 fn main() {
     part1();
