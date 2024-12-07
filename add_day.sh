6fd9407dd7b707cbde2186754ac03c1ebab6e5c5
Ben Dayan <ben.dayan@gehealthcare.com>
2024-12-07 16:21:35 +0200

    template: have initial template contain part1, part2 and read lines

--- add_day.sh
+++ add_day.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
 DAY=$(printf "%02d" $1)
-CRATE=day${DAY}
+export CRATE=day${DAY}
 
 # add crate to workspace members
 sed -i -e '/# placeholder/i "'${CRATE}'",' Cargo.toml
@@ -9,6 +9,9 @@ sed -i -e '/# placeholder/i "'${CRATE}'",' Cargo.toml
 # create new crate
 cargo new $CRATE
 cargo add --package ${CRATE} --path common
+
+# update the base empty template
+cat empty_day.rs.template | envsubst > ${CRATE}/src/main.rs
 
 # check that things build
 cargo build
