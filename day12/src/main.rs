eba474f916ee5fe38f9e9725d647c12deca2fb18
Ben Dayan <ben.dayan@gehealthcare.com>
2024-12-20 16:57:48 +0200

    feat: adding day12

---
+++ day12/src/main.rs
@@ -0,0 +1,13 @@
+use common::read_lines;
+
+fn part1() {
+    let lines = read_lines("./day12/input").unwrap().collect::<Vec<_>>();
+}
+
+fn part2() {
+    let lines = read_lines("./day12/input").unwrap().collect::<Vec<_>>();
+}
+fn main() {
+    part1();
+    part2()
+}
